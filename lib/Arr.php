c28d47270454945e3eb951aa342fb36131dd0188
Daniel Lucks <daniel.lucks@commerce-plus.com>
2016-06-24 19:09:42 +0200

    Add Getting Values from Multidimensional Arrays

--- lib/Arr.php
+++ lib/Arr.php
@@ -1,40 +1,81 @@
 <?php
 
-class Arr {
+class Arr
+{
+    /**
+     * Separator for multi keys accessing values
+     * in multidimensional arrays.
+     */
+    const MULTI_KEY_SEPARATOR = '.';
 
-	public static function get($array, $key, $default = null)
-	{
+    /**
+     * Get a value from an array. If given array is
+     * multidimensional, a multi-key can be given to
+     * access nested array fields.
+     *
+     * A multi key can be something like 'key1.key2'.
+     *
+     * @param array $array
+     * @param string $key
+     * @param null $default
+     *
+     * @return mixed
+     */
+    public static function get($array, $key, $default = null)
+    {
         if (!is_array($array)) {
             return $default;
         }
 
-		return (array_key_exists($key, $array) ? $array[$key] : $default);
-	}
+        if (strpos($key, self::MULTI_KEY_SEPARATOR) !== false) {
 
-	public static function isTraversable($value)
-    {
-        // Obviously an array is traversable.
-        if (is_array($value)) {
-            return true;
+            $multiKey   = explode(self::MULTI_KEY_SEPARATOR, $key);
+            $firstKey   = array_shift($multiKey);
+            $innerArray = Arr::get($array, $firstKey);
 
-        // If it is an object implementing the `Traversable` interface, it is also traversable.
-        } else {
-            return (is_object($value) && ($value instanceof Traversable));
+            return Arr::get($innerArray, implode(self::MULTI_KEY_SEPARATOR, $multiKey), $default);
         }
+
+        return (array_key_exists($key, $array) ? $array[$key] : $default);
     }
 
-	public static function removeValues($array, $values)
-	{
-		if (!is_array($values)) {
-			$values = array($values);
-		}
+    /**
+     * Check if given variable is traversable.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public static function isTraversable($value)
+    {
+        if (is_array($value)) {
+            return true;
+        }
 
-		foreach ($array as $key => $value) {
-			if (array_search($value, $values) !== false) {
-				unset($array[$key]);
-			}
-		}
+        return (is_object($value) && ($value instanceof Traversable));
+    }
 
-		return $array;
-	}
+    /**
+     * Remove a given set of values from an array
+     * and return it.
+     *
+     * @param array $array
+     * @param array $values
+     *
+     * @return array
+     */
+    public static function removeValues($array, $values)
+    {
+        if (!is_array($values)) {
+            $values = array($values);
+        }
+
+        foreach ($array as $key => $value) {
+            if (array_search($value, $values) !== false) {
+                unset($array[$key]);
+            }
+        }
+
+        return $array;
+    }
 }
